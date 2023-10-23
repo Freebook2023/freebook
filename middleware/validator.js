8dc97b34f525f0e24831ab0b20420fbb9e47e5fa
Freebook2023 <147560721+Freebook2023@users.noreply.github.com>
2023-10-23 19:02:24 +0100

    Update validator.js

--- middleware/validator.js
+++ middleware/validator.js
@@ -3,13 +3,13 @@ const Joi = require("joi");
 exports.userValidate = async (req, res, next) => {
   try {
     const userSchema = Joi.object({
-      fullName: Joi.string().trim().min(5),
-       
-      whichSocialMedia: Joi.string().trim(),
+      fullName: Joi.string().trim().min(5).required(),
+      email: Joi.string().email().trim().required(),
+      whichSocialMedia: Joi.string().trim().required(),
     })
-   .with("fullName", [ "whichSocialMedia"]);
+    .with("fullName", ["email", "whichSocialMedia"]);
     await userSchema.validateAsync(req.body);
-    next() 
+    next()
   } catch (err) {
     res.status(400).json({
       status: 400,
