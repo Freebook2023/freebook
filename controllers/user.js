8216fee131cf824e688fad54b8bd019930ac71d9
Freebook2023 <147560721+Freebook2023@users.noreply.github.com>
2023-10-24 19:41:55 +0100

    Update user.js

--- controllers/user.js
+++ controllers/user.js
@@ -4,7 +4,7 @@ exports.userDetail = async (req, res, next) => {
   try{
     const user = await new UserData(req.body);
     await user.save();
-    return res.status(200).sendFile(path.join(__dirname, '../', 'files', 'How-to-Sell-to-Nigerians.pdf'))
+    return res.status(200).sendFile(path.join(__dirname, '../', 'files', 'PRICING.pdf'))
   }catch(err){
     return res.status(400).json({
       status: 400,
